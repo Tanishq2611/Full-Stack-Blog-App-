f87fa3c4955b7e474a5005cef257a0badf6442aa
Tanishq2611 <66460570+Tanishq2611@users.noreply.github.com>
2025-08-25 22:52:08 +0530

    Update Featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,9 +6,9 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey, UNLEASED BLOGGER here!</b> Discover my stories and creative ideas.
+        <b>Hey, Narrato BLOGGER here!</b> Discover my stories and creative ideas.
       </h1>
-      <div className={styles.post}>
+      <div className={styles.post}> 
         <div className={styles.imgContainer}>
           <Image src="/p1.jpeg" alt="" fill className={styles.image} />
         </div>
